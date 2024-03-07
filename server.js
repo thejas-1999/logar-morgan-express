ba3b60c95302d8633ef387599e8e1b1d1b295073
thejas-1999 <thejask074@gmail.com>
2024-03-07 13:33:54 +0530

    create a specific file to store the details

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(morgan(`:method :status :url "HTTP/:http-version" `));
+morgan.token(
+  "id",
+  (getId = (req) => {
+    return req.id;
+  })
+);
 
+const assignedId = (req, res, next) => {
+  req.id = uuidv4();
+  next();
+};
+
+app.use(assignedId);
+
+let accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "accessLog.log"),
+  { flag: "a" }
+);
+
+app.use(morgan(`:id :method `, { stream: accessLogStream }));
+
+app.use(morgan(`:id  `));
 app.get("/", (req, res) => {
   res.send("Morgan Loggar Application");
 });
